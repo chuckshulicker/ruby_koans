d3ed0b9035d6e0b35f4392463319e8e4917dbb2f
Chuck Shulicker <where.charlie.routes.accounts@gmail.com>
2012-05-28 09:12:38 -0400

    159/280

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -13,8 +13,23 @@
 # and
 #   about_triangle_project_2.rb
 #
+
+# trying to be fancy here
+# def triangle(*args)
+# splat for an array just because
+#
+# sides = args.is_a?(Array) ? args : args.to_a
+# do an interesection, result is a count of unequal sides
+# unequal_sides = (sides & sides).length
+
 def triangle(a, b, c)
-  # WRITE THIS CODE
+	# triangle error handling		
+	raise TriangleError, "Sides must be greater than zero." if a <= 0 || b <= 0 || c <= 0
+	raise TriangleError, "Sum of two sides can not be less than or equal to it's remaining side." if a+b <= c || a+c <= b || b+c <= a
+	# triangle return logic		
+	return :equilateral if a == b && b == c
+	return :isosceles if (a == b && a != c) || (a == c && a != b) || (b == c && b != a)
+	return :scalene if a != b && b != c
 end
 
 # Error class used in part 2.  No need to change this code.
